7545288e4d373f1f34711db8f834ca6fde21c13d
Kim Burgestrand <kim@burgestrand.se>
2012-08-01 04:17:22 +0200

    Make naming scheme for binary much easier

--- lib/libspotify.rb
+++ lib/libspotify.rb
@@ -1,14 +1,1 @@
-require 'rbconfig'
-
-gempath = File.dirname(__FILE__)
-suffix  = case RbConfig::CONFIG["host_os"].downcase
-when /darwin/
-  "dylib"
-when /mingw|mswin/
-  "dll"
-else
-  # not windows or mac os, we assume linuxy
-  "so"
-end
-
-LIBSPOTIFY_BIN = File.expand_path("../libspotify.#{suffix}", gempath)
+LIBSPOTIFY_BIN = File.expand_path("../libspotify", File.dirname(__FILE__))
